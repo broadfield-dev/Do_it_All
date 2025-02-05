b06309bfd6d2d7a3119977c30e1ad300429379f6
broadfield-dev <broadfield.apps@gmail.com>
2025-02-05 10:45:28 -0800

    Create chat_style.py

---
+++ doitall/addons/chat_style.py
@@ -0,0 +1,22 @@
+user="""
+    background: #cccbcb;
+    padding: 20px;
+    padding-top: 50px;
+    font-size: 14px;
+    text-align: right;
+    box-shadow: 0px 3px #00000085;
+    font-family: system-ui;
+    font-weight: 500;
+    color: #14042d;
+"""
+bot="""
+    background: #14042d;
+    padding: 20px;
+    font-size: 14px;
+    text-align: left;
+    box-shadow: 0px 3px #00000085;
+    font-family: system-ui;
+    font-weight: 500;
+    color: #cccbcb;
+}
+"""
